3d6906a2553ccac1c59a9b751a256b4e1a121da6
Javierdr <jidreves@uc.cl>
2021-05-30 03:18:44 -0400

    feature: camera view for plate recognition

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import PlatesPage from '@/components/PlatesPage'
 
 Vue.use(Router)
 
+
 export default new Router({
+  mode : 'history',
   routes: [
     {
       path: '/',
       name: 'HomePage',
       component: HomePage
+    },
+    {
+      path: '/plates',
+      name: 'PlatesPage',
+      component: PlatesPage
     }
   ]
 })
