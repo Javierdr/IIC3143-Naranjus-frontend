19271b4b71f72c63d742ea6a5db2e4e66c68f813
Calomi <cierrios@uc.cl>
2021-05-30 04:56:09 -0400

    router ambas vistas registro de vistas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import registro_visitas from '@/views/registro_visitas'
+import visitas_registradas from '@/views/visitas_registradas'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HomePage',
       component: HomePage
-    }
+    },
+    {
+      path: '/registro_visitas',
+      name: 'registro_visitas',
+      component: registro_visitas
+    },
+    {
+      path: '/visitas_registradas',
+      name: 'visitas_registradas',
+      component: visitas_registradas
+    },
   ]
 })
