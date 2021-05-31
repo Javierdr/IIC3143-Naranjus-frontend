2fc9bbba1e75bf4787dc73b59b6e6304be8d4415
Calomi <cierrios@uc.cl>
2021-05-31 18:09:12 -0400

    añadiendo bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App';
 import router from './router';
-import Vuex from 'vuex'
-import { store } from './store'
+import Vuex from 'vuex';
+import {store} from './store';
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
 
-Vue.config.productionTip = false
-Vue.use(Vuex)
+Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 /* eslint-disable no-new */
 new Vue({
@@ -15,4 +23,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-})
+});
