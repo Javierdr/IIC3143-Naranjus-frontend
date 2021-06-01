84439f266b64d3b153056ffadda25c9a81010e2b
Calomi <cierrios@uc.cl>
2021-05-31 22:51:00 -0400

    link en menu a visitas programadas

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,12 +2,14 @@
   <div>
 
     <h1> Bienvenido a Naranjus Security Solutions, usuario </h1>
-    
+
     <ul class="list-group">
     <li class="list-group-item"><router-link to="plates">Camaras </router-link></li>
-    <li class="list-group-item"><router-link to="registro_visitas">Registro de visitas </router-link></li> 
-    <li class="list-group-item"><router-link to="visitas_registradas">Visitas Registradas </router-link></li> 
-   
+    <li class="list-group-item"><router-link to="programar_visitas">Programar visitas</router-link></li>
+    <li class="list-group-item"><router-link to="visitas_programadas">Visitas Programadas </router-link></li>
+    <li class="list-group-item"><router-link to="registro_visitas">Registro de visitas </router-link></li>
+    <li class="list-group-item"><router-link to="visitas_registradas">Visitas Registradas </router-link></li>
+
     </ul>
 
   </div>
