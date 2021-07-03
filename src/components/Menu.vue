be029f13433da72faacda5d4d41767ae713c53c4
Seld0nn <37000553+Seld0nn@users.noreply.github.com>
2021-07-03 12:26:48 -0400

    navbar avance

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,26 +1,66 @@
 <template>
-  <div>
-    
-    <h1> Bienvenido a Naranjus Security Solutions, usuario </h1>
-    <img src="assets/logonombre.png" class="img-fluig">
-    <ul class="list-group">
-    <li class="list-group-item"><router-link to="plates">Camaras </router-link></li>
-    <li class="list-group-item"><router-link to="programar_visitas">Programar visitas</router-link></li>
-    <li class="list-group-item"><router-link to="visitas_programadas">Visitas Programadas </router-link></li>
-    <li class="list-group-item"><router-link to="registro_visitas">Registro de visitas </router-link></li>
-    <li class="list-group-item"><router-link to="visitas_registradas">Visitas Registradas </router-link></li>
-    <li class="list-group-item"><router-link to="logs_entrada_general">Logs entrada general </router-link></li>
-    </ul>
 
-  </div>
+  <div id="naranjus_navbar">
+  <b-navbar toggleable="lg" type="dark" variant="danger" class="navbar-horizontal">
+
+    <b-navbar-brand to="/" class="nav-link active" href="#">Naranjus</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <li class="nav-item">
+          <router-link to="/LogsEntrada" class="nav-link active" arial-currente="page">Residentes</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link  to="/LogsEntradaVU" class="nav-link active" arial-currente="page">Visitas</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="plates" class="nav-link active" arial-currente="page">Nuevo Usuario</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/PlatesPage" class="nav-link active" arial-currente="page">Patentes</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/Register" class="nav-link active" arial-currente="page">Registro</router-link>
+        </li>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <li class="nav-item">
+
+            <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+         </li>
+
+         <li class="nav-item">
+              <router-link to="/Login" class="nav-link active" arial-currente="page">Login</router-link>
+         </li>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+
 
 </template>
+
+<router-view></router-view>
 
 
 <script>
 export default {
     data: {
-      
+
     }
 
 }
@@ -36,6 +76,6 @@ export default {
 }
 
 .router-link {
-  color: 
+  color:
 }
 </style>
