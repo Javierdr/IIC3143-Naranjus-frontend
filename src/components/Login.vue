73a1fafd13a0c8bccb02b5dc5eda7148ec628567
Javierdr <jidreves@uc.cl>
2021-07-05 01:50:44 -0400

    login hotfix

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -64,7 +64,7 @@ export default {
         },
         registerButton: async function(){
             this.$router.push('Register');
-
+        },
         setCurrentUser() {
           this.$store.dispatch('setCurrentUserAction', localStorage.access)
         }
